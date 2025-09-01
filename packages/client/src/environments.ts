87546fc26abcc914762dbf6fbf09d28bb4a6f6a3
mark hinschberger <markhinschberger@gmail.com>
2025-09-01 12:36:48 +0100

    chore: delete me actions

--- packages/client/src/environments.ts
+++ packages/client/src/environments.ts
@@ -13,7 +13,7 @@ export type EnvironmentConfig = {
  */
 export const production: EnvironmentConfig = {
   name: 'production',
-  backend: 'https://api.v3.aave.com/graphql',
+  backend: 'https://api.v4.aave.com/graphql',
   indexingTimeout: 60_000,
   pollingInterval: 100,
 };
@@ -23,7 +23,7 @@ export const production: EnvironmentConfig = {
  */
 export const staging: EnvironmentConfig = {
   name: 'staging',
-  backend: 'https://api.v3.staging.aave.com/graphql',
+  backend: 'https://api.v4.staging.aave.com/graphql',
   indexingTimeout: 60_000,
   pollingInterval: 100,
 };
