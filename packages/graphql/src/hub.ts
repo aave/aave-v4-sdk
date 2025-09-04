d538d87f06bef36fa067fe7454cc52cfa5d9f3a4
mark hinschberger <markhinschberger@gmail.com>
2025-09-04 09:52:58 +0100

    fix: cosolidate hub

--- packages/graphql/src/hub.ts
+++ packages/graphql/src/hub.ts
@@ -1,15 +1,42 @@
 import { HubAssetFragment } from './fragments';
+import { HubFragment } from './fragments/hubs';
 import { graphql, type RequestOf } from './graphql';
+
+/**
+ * @internal
+ */
+export const HubQuery = graphql(
+  `query Hub($request: HubRequest!) {
+      value: hub(request: $request) {
+        ...Hub
+      }
+    }`,
+  [HubFragment],
+);
+export type HubRequest = RequestOf<typeof HubQuery>;
+
+/**
+ * @internal
+ */
+export const HubsQuery = graphql(
+  `query Hubs($request: HubsRequest!) {
+      value: hubs(request: $request) {
+        ...Hub
+      }
+    }`,
+  [HubFragment],
+);
+export type HubsRequest = RequestOf<typeof HubsQuery>;
 
 /**
  * @internal
  */
 export const HubAssetsQuery = graphql(
   `query HubAssets($request: HubAssetsRequest!, $currency: Currency!) {
-    value: hubAssets(request: $request) {
-      ...HubAsset
-    }
-  }`,
+      value: hubAssets(request: $request) {
+        ...HubAsset
+      }
+    }`,
   [HubAssetFragment],
 );
 export type HubAssetsRequest = RequestOf<typeof HubAssetsQuery>;
