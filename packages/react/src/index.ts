da7b92943c86d8cd5fddac50e241c9533045927b
mark hinschberger <markhinschberger@gmail.com>
2025-09-04 14:11:09 +0100

    feat: permit

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -4,6 +4,7 @@ export * from './AaveProvider';
 export { useAaveClient } from './context';
 export * from './hubs';
 export * from './misc';
+export * from './permits';
 export * from './reserves';
 export * from './swap';
 export * from './transactions';
