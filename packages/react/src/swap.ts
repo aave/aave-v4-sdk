5906e27182957491cf32bef9fdeb600c004eb664
Juan <juan@avara.xyz>
2025-09-04 10:50:15 +0200

    feat: add support for swappableTokens query (#19)

--- packages/react/src/swap.ts
+++ packages/react/src/swap.ts
@@ -1,7 +1,19 @@
 import { swapQuote } from '@aave/client-next';
 import type { UnexpectedError } from '@aave/core-next';
 import type { SwapQuote, SwapQuoteRequest } from '@aave/graphql-next';
+import {
+  SwappableTokensQuery,
+  type SwappableTokensRequest,
+  type Token,
+} from '@aave/graphql-next';
 import { useAaveClient } from './context';
+import {
+  type ReadResult,
+  type Suspendable,
+  type SuspendableResult,
+  type SuspenseResult,
+  useSuspendableQuery,
+} from './helpers';
 import { type UseAsyncTask, useAsyncTask } from './helpers/tasks';
 
 /**
@@ -41,4 +53,50 @@ export function useSwapQuote(): UseAsyncTask<
   return useAsyncTask((request: SwapQuoteRequest) =>
     swapQuote(client, request),
   );
+}
+
+export type UseSwappableTokensArgs = SwappableTokensRequest;
+
+/**
+ * Fetch the list of tokens available for swapping on a specific chain.
+ *
+ * This signature supports React Suspense:
+ *
+ * ```tsx
+ * const { data } = useSwappableTokens({
+ *   query: { chainId: chainId(1) },
+ *   suspense: true,
+ * });
+ * ```
+ */
+export function useSwappableTokens(
+  args: UseSwappableTokensArgs & Suspendable,
+): SuspenseResult<Token[]>;
+
+/**
+ * Fetch the list of tokens available for swapping on a specific chain.
+ *
+ * ```tsx
+ * const { data, error, loading } = useSwappableTokens({
+ *   query: { chainId: chainId(1) },
+ * });
+ * ```
+ */
+export function useSwappableTokens(
+  args: UseSwappableTokensArgs,
+): ReadResult<Token[]>;
+
+export function useSwappableTokens({
+  suspense = false,
+  ...request
+}: UseSwappableTokensArgs & {
+  suspense?: boolean;
+}): SuspendableResult<Token[]> {
+  return useSuspendableQuery({
+    document: SwappableTokensQuery,
+    variables: {
+      request,
+    },
+    suspense,
+  });
 }
