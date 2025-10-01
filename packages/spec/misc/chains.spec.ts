2483fd6d7232c47bd6ed56ae865e53129e38270b
Juan Garcia <juan@avara.xyz>
2025-10-01 10:15:13 +0200

    test: chain and hubs scenarios

---
+++ packages/spec/misc/chains.spec.ts
@@ -0,0 +1,20 @@
+import { assertOk, ChainsFilter } from '@aave/client-next';
+import { chains } from '@aave/client-next/actions';
+import { client } from '@aave/client-next/test-utils';
+import { describe, expect, it } from 'vitest';
+
+describe('Aave V4 Chains Scenario', () => {
+  describe('Given a user who wants to list available chains', () => {
+    describe('When listing supported chains with a filter', () => {
+      const filter = Object.values(ChainsFilter);
+      it.each(filter)(
+        'Then it should return the expected list chains with filter %s',
+        async (filter) => {
+          const result = await chains(client, filter);
+          assertOk(result);
+          expect(result.value).toMatchSnapshot();
+        },
+      );
+    });
+  });
+});
---
+++ packages/spec/misc/hub.spec.ts
@@ -0,0 +1,101 @@
+import { assertOk, chainId } from '@aave/client-next';
+import { hub, hubAssets, hubs } from '@aave/client-next/actions';
+import {
+  client,
+  ETHEREUM_USDC_ADDRESS,
+  ETHEREUM_WETH_ADDRESS,
+} from '@aave/client-next/test-utils';
+import { describe, expect, it } from 'vitest';
+import { assertNonEmptyArray } from '../test-utils';
+
+describe('Aave V4 Hub Scenarios', () => {
+  describe('Given a user who wants to list available hubs', () => {
+    describe('When fetching hubs by chain ID(s)', () => {
+      it('Then it should return the expected data for each hub', async () => {
+        const result = await hubs(client, {
+          query: { chainIds: [chainId(1)] },
+        });
+        assertOk(result);
+        result.value.forEach((hub) => {
+          expect(hub).toMatchSnapshot({
+            summary: expect.any(Object),
+          });
+        });
+      });
+    });
+
+    describe('When fetching hubs by tokens)', () => {
+      const tokens = [ETHEREUM_USDC_ADDRESS, ETHEREUM_WETH_ADDRESS];
+
+      it('Then the list of hubs should contains assets from the query', async () => {
+        const listHubs = await hubs(client, {
+          query: {
+            tokens: tokens.map((token) => ({
+              address: token,
+              chainId: chainId(1),
+            })),
+          },
+        });
+        assertOk(listHubs);
+
+        for (const hub of listHubs.value) {
+          const result = await hubAssets(client, {
+            chainId: chainId(1),
+            hub: hub.address,
+          });
+          assertOk(result);
+          const assetsInHub = result.value.map(
+            (asset) => asset.underlying.address,
+          );
+          expect(tokens.some((token) => assetsInHub.includes(token))).toBe(
+            true,
+          );
+        }
+      });
+    });
+  });
+
+  describe('Given a user who wants to fetch a single hub', () => {
+    describe('When fetching a single hub', () => {
+      it('Then it should return the expected data for the hub', async () => {
+        const listHubs = await hubs(client, {
+          query: {
+            chainIds: [chainId(1)],
+          },
+        });
+        assertOk(listHubs);
+        assertNonEmptyArray(listHubs.value);
+
+        const result = await hub(client, {
+          hub: listHubs.value[0].address,
+          chainId: chainId(1),
+        });
+        assertOk(result);
+        expect(result.value).toMatchObject(listHubs.value[0]);
+      });
+    });
+  });
+
+  describe('Given a user who wants to know assets in a hub', () => {
+    describe('When fetching assets in a hub', () => {
+      it('Then it should return the expected data for assets in a hub', async () => {
+        const listHubs = await hubs(client, {
+          query: {
+            chainIds: [chainId(1)],
+          },
+        });
+
+        assertOk(listHubs);
+        assertNonEmptyArray(listHubs.value);
+
+        const result = await hubAssets(client, {
+          hub: listHubs.value[0].address,
+          chainId: chainId(1),
+        });
+        assertOk(result);
+        assertNonEmptyArray(result.value);
+        expect(result.value).toMatchObject(listHubs.value[0]);
+      });
+    });
+  });
+});
