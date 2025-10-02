e61fe3e80380e7a29dbd4f8d26af430402d695f2
Cesare Naldi <naldi.cesare@gmail.com>
2025-10-02 19:36:14 +0200

    chore: removes SpokeConfig ahead of breaking change

--- packages/graphql/src/fragments/spoke.ts
+++ packages/graphql/src/fragments/spoke.ts
@@ -2,14 +2,6 @@ import type { FragmentOf } from 'gql.tada';
 import { graphql } from '../graphql';
 import { ChainFragment, PaginatedResultInfoFragment } from './common';
 
-export const SpokeConfigFragment = graphql(
-  `fragment SpokeConfig on SpokeConfig {
-    __typename
-    active
-  }`,
-);
-export type SpokeConfig = FragmentOf<typeof SpokeConfigFragment>;
-
 export const SpokeFragment = graphql(
   `fragment Spoke on Spoke {
     __typename
@@ -18,11 +10,8 @@ export const SpokeFragment = graphql(
     chain {
       ...Chain
     }
-    config {
-      ...SpokeConfig
-    }
   }`,
-  [ChainFragment, SpokeConfigFragment],
+  [ChainFragment],
 );
 
 export type Spoke = FragmentOf<typeof SpokeFragment>;
