f2fd77df8d0bd7553a3bfa762e006492a1a5279e
Juan Garcia <juan@avara.xyz>
2025-10-09 11:45:41 +0200

    fix: refactor scenario to borrow

--- packages/spec/borrow/business.spec.ts
+++ packages/spec/borrow/business.spec.ts
@@ -1,58 +1,53 @@
 import { assertOk, bigDecimal, evmAddress } from '@aave/client-next';
-import { borrow, userBorrows, userSupplies } from '@aave/client-next/actions';
+import { borrow, userBorrows } from '@aave/client-next/actions';
 import {
   client,
   createNewWallet,
-  ETHEREUM_USDC_ADDRESS,
+  ETHEREUM_GHO_ADDRESS,
+  ETHEREUM_WETH_ADDRESS,
   fundErc20Address,
 } from '@aave/client-next/test-utils';
 import { sendWith } from '@aave/client-next/viem';
-import type { Reserve } from '@aave/graphql-next';
 import { beforeAll, describe, expect, it } from 'vitest';
 import { assertSingleElementArray } from '../test-utils';
-import { supplyToRandomERC20Reserve } from './helper';
+import { findReserveToBorrow, supplyToRandomERC20Reserve } from './helper';
 
 const user = await createNewWallet();
 
 describe('Aave V4 Borrow Scenarios', () => {
   describe('Given a user with a supply position as collateral', () => {
     describe('When the user borrows an ERC20 asset', () => {
-      let reserve: Reserve;
-
       beforeAll(async () => {
         const setup = await fundErc20Address(evmAddress(user.account.address), {
-          address: ETHEREUM_USDC_ADDRESS,
-          amount: bigDecimal('300'),
-          decimals: 6,
+          address: ETHEREUM_WETH_ADDRESS,
+          amount: bigDecimal('1.0'),
         }).andThen(() =>
-          supplyToRandomERC20Reserve(client, user, ETHEREUM_USDC_ADDRESS),
+          supplyToRandomERC20Reserve(client, user, {
+            token: ETHEREUM_WETH_ADDRESS,
+            amount: bigDecimal('0.5'),
+          }),
         );
 
         assertOk(setup);
-        reserve = setup.value;
+        console.log(`Supply to reserve: ${setup.value.spoke.address}`);
       });
 
       it(`Then the user's borrow positions are updated`, async () => {
-        const position = await userSupplies(client, {
-          query: {
-            userChains: {
-              chainIds: [reserve.chain.chainId],
-              user: evmAddress(user.account.address),
-            },
-          },
-        });
-
-        assertOk(position);
-        assertSingleElementArray(position.value);
+        const reserveToBorrow = await findReserveToBorrow(
+          client,
+          user,
+          ETHEREUM_GHO_ADDRESS,
+        );
+        assertOk(reserveToBorrow);
         const amountToBorrow =
-          position.value[0].reserve.userState!.borrowable.value.formatted;
+          reserveToBorrow.value.userState!.borrowable.value.formatted;
 
         const result = await borrow(client, {
           sender: evmAddress(user.account.address),
           reserve: {
-            spoke: reserve.spoke.address,
-            reserveId: reserve.id,
-            chainId: reserve.chain.chainId,
+            spoke: reserveToBorrow.value.spoke.address,
+            reserveId: reserveToBorrow.value.id,
+            chainId: reserveToBorrow.value.chain.chainId,
           },
           amount: {
             erc20: {
@@ -67,8 +62,8 @@ describe('Aave V4 Borrow Scenarios', () => {
               query: {
                 userSpoke: {
                   spoke: {
-                    address: reserve.spoke.address,
-                    chainId: reserve.chain.chainId,
+                    address: reserveToBorrow.value.spoke.address,
+                    chainId: reserveToBorrow.value.chain.chainId,
                   },
                   user: evmAddress(user.account.address),
                 },
