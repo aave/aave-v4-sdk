82f9ae7dc6f3737245650c370949cf62cc81bbc5
Juan <juan@avara.xyz>
2025-10-13 10:39:41 +0200

    test: set supply as collateral (#100)

--- packages/spec/collateralToggle.todo.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it } from 'vitest';
-
-describe('Aave V4 Collateral Toggle Scenario', () => {
-  describe('Given a user with a supply position', () => {
-    describe('When the user toggles the position as collateral', () => {
-      it.todo('Then it should be reflected in the user supply positions');
-    });
-  });
-});
