e77f68deb5fef2edabbbf98d1a793ca795e29a6f
Juan <juan@avara.xyz>
2025-10-14 15:50:43 +0200

    test: userSummary query (#107)

---
+++ packages/spec/positions/userSummary.spec.ts
@@ -0,0 +1,121 @@
+import { assertOk, Currency, evmAddress, TimeWindow } from '@aave/client-next';
+import { userSummary } from '@aave/client-next/actions';
+import {
+  client,
+  createNewWallet,
+  ETHEREUM_FORK_ID,
+  ETHEREUM_SPOKE_CORE_ADDRESS,
+} from '@aave/client-next/test-utils';
+import { beforeAll, describe, expect, it } from 'vitest';
+
+const user = await createNewWallet(
+  '0x78cb95218f49e3bf57fc7862de83c079da46c2055c433901e902dcd4a7d39ff0',
+);
+
+describe('Querying User Summary on Aave V4', () => {
+  describe('Given a user with multiple active positions', () => {
+    beforeAll(async () => {
+      // NOTE: Enable when needed to create positions for a new user
+      // const setup = await fundErc20Address(evmAddress(user.account.address), {
+      //   address: ETHEREUM_WETH_ADDRESS,
+      //   amount: bigDecimal('0.5'),
+      // })
+      //   .andThen(() =>
+      //     fundErc20Address(evmAddress(user.account.address), {
+      //       address: ETHEREUM_WSTETH_ADDRESS,
+      //       amount: bigDecimal('0.5'),
+      //     }),
+      //   )
+      //   // .andThen(() => supplyWETHAndBorrow(client, user, ETHEREUM_USDS_ADDRESS))
+      //   .andThen(() => supplyWSTETHAndBorrowETH(client, user));
+      // assertOk(setup);
+    }, 120_000);
+
+    describe('When the user queries their summary without filters', () => {
+      it('Then the summary with aggregated financial data is returned', async () => {
+        const summary = await userSummary(client, {
+          user: evmAddress(user.account.address),
+        });
+        assertOk(summary);
+        expect(summary.value.totalPositions).toBe(2);
+      });
+    });
+
+    describe('When the user queries their summary filtered by spoke', () => {
+      it('Then the summary for that specific spoke is returned', async () => {
+        const summary = await userSummary(client, {
+          user: evmAddress(user.account.address),
+          filter: {
+            spoke: {
+              address: ETHEREUM_SPOKE_CORE_ADDRESS,
+              chainId: ETHEREUM_FORK_ID,
+            },
+          },
+        });
+        assertOk(summary);
+        expect(summary.value.totalPositions).toBe(1);
+      });
+    });
+
+    describe('When the user queries their summary filtered by chain IDs', () => {
+      it('Then the summary for those specific chains is returned', async () => {
+        const summary = await userSummary(client, {
+          user: evmAddress(user.account.address),
+          filter: {
+            chainIds: [ETHEREUM_FORK_ID],
+          },
+        });
+        assertOk(summary);
+        expect(summary.value.totalPositions).toBe(2);
+      });
+    });
+
+    describe('When the user queries their summary with different currency options', () => {
+      it('Then the summary values are returned in the specified currency', async () => {
+        const summaryEUR = await userSummary(
+          client,
+          {
+            user: evmAddress(user.account.address),
+          },
+          { currency: Currency.Eur },
+        );
+        assertOk(summaryEUR);
+        expect(summaryEUR.value.netBalance.amount.name).toBe('EUR');
+        expect(summaryEUR.value.totalCollateral.name).toBe('EUR');
+        expect(summaryEUR.value.totalSupplied.name).toBe('EUR');
+        expect(summaryEUR.value.totalDebt.name).toBe('EUR');
+
+        const summaryGBP = await userSummary(
+          client,
+          {
+            user: evmAddress(user.account.address),
+          },
+          { currency: Currency.Gbp },
+        );
+        assertOk(summaryGBP);
+        expect(summaryGBP.value.netBalance.amount.name).toBe('GBP');
+        expect(summaryGBP.value.totalCollateral.name).toBe('GBP');
+        expect(summaryGBP.value.totalSupplied.name).toBe('GBP');
+        expect(summaryGBP.value.totalDebt.name).toBe('GBP');
+      });
+    });
+
+    describe('When the user queries their summary with different time windows', () => {
+      const timeWindowOptions = Object.values(TimeWindow);
+      it.each(timeWindowOptions)(
+        'Then the summary is returned for the %s time window',
+        async (timeWindow) => {
+          const summary = await userSummary(
+            client,
+            {
+              user: evmAddress(user.account.address),
+            },
+            { timeWindow: timeWindow },
+          );
+          assertOk(summary);
+          // Note: think about how to make sure the summary is returned for the correct time window
+        },
+      );
+    });
+  });
+});
