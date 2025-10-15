27d681d633626ad3b3e939e2bd99932d1022eadc
Juan <juan@avara.xyz>
2025-10-15 14:41:01 +0200

    test: borrow and supply APY history queries (#110)

---
+++ packages/spec/liquidity/supplyApy.spec.ts
@@ -0,0 +1,48 @@
+import { assertOk, TimeWindow } from '@aave/client-next';
+import { reserves, supplyApyHistory } from '@aave/client-next/actions';
+import {
+  client,
+  ETHEREUM_FORK_ID,
+  ETHEREUM_SPOKE_CORE_ADDRESS,
+  ETHEREUM_USDC_ADDRESS,
+} from '@aave/client-next/test-utils';
+import { describe, it } from 'vitest';
+import { assertNonEmptyArray } from '../test-utils';
+
+// TODO: Enable and refactor after fixed AAVE-2159
+describe.skip('Supply APY History on Aave V4', () => {
+  describe('Given a reserve with supply activity', () => {
+    describe('When fetching supply APY history with different time windows', () => {
+      const timeWindowOptions = Object.values(TimeWindow);
+
+      it.each(timeWindowOptions)(
+        'Then it should return supply APY history for %s time window',
+        async (window) => {
+          // Get a reserve to test with
+          const usdcReserve = await reserves(client, {
+            query: {
+              spokeToken: {
+                token: ETHEREUM_USDC_ADDRESS,
+                spoke: ETHEREUM_SPOKE_CORE_ADDRESS,
+                chainId: ETHEREUM_FORK_ID,
+              },
+            },
+          });
+          assertOk(usdcReserve);
+          assertNonEmptyArray(usdcReserve.value);
+
+          const result = await supplyApyHistory(client, {
+            spoke: {
+              address: usdcReserve.value[0].spoke.address,
+              chainId: usdcReserve.value[0].spoke.chain.chainId,
+            },
+            reserve: usdcReserve.value[0].id,
+            window: window,
+          });
+
+          assertOk(result);
+        },
+      );
+    });
+  });
+});
