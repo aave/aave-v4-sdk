37f5b913f4ed1e2b859c6ecf92e61a0b59887418
Juan Garcia <juan@avara.xyz>
2025-11-13 12:36:23 +0100

    fix: remove conversions from BigDecimal to Number

--- vitest.d.ts
+++ vitest.d.ts
@@ -3,7 +3,6 @@ import 'vitest';
 declare module 'vitest' {
   interface AsymmetricMatchersContaining extends JestExtendedMatchers {
     toBeBigDecimalCloseTo: (expected: number | string, precision?: number) => R;
-    toBeBigDecimalWithin: (start: number | string, end: number | string) => R;
     toBeBigDecimalGreaterThan: (expected: number | string) => R;
     toBeBigDecimalLessThan: (expected: number | string) => R;
     toBeBetweenDates: (start: Date, end: Date) => R;
