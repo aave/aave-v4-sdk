1dddad2ac9f57853c7cc05ab297e8d8d2c1c8c12
Juan Garcia <juan@aave.com>
2025-12-12 12:35:18 +0100

    chore: relocate asset tests in a separate folder

--- packages/spec/liquidity/asset/assetBorrowHistory.spec.ts
+++ packages/spec/liquidity/asset/assetBorrowHistory.spec.ts
@@ -1,18 +1,18 @@
 import { assertOk, TimeWindow } from '@aave/client';
-import { assetSupplyHistory } from '@aave/client/actions';
+import { assetBorrowHistory } from '@aave/client/actions';
 import {
   client,
   ETHEREUM_FORK_ID,
   ETHEREUM_USDC_ADDRESS,
 } from '@aave/client/testing';
 import { describe, expect, it } from 'vitest';
-import { getTimeWindowDates } from '../helpers/tools';
+import { getTimeWindowDates } from '../../helpers/tools';
 
-describe('Querying Asset Supply Apy History on Aave V4', () => {
+describe('Querying Asset Apy Borrow History on Aave V4', () => {
   describe('Given an asset/token available on the protocol', () => {
-    describe('When fetching the asset apy supply history', () => {
-      it('Then it should return the asset apy supply history', async () => {
-        const result = await assetSupplyHistory(client, {
+    describe('When fetching the asset apy borrow history', () => {
+      it('Then it should return the asset apy borrow history', async () => {
+        const result = await assetBorrowHistory(client, {
           query: {
             token: {
               chainId: ETHEREUM_FORK_ID,
@@ -22,9 +22,10 @@ describe('Querying Asset Supply Apy History on Aave V4', () => {
         });
 
         assertOk(result);
+        expect(result.value).toBeArray();
         expect(result.value).toBeArrayWithElements(
           expect.objectContaining({
-            __typename: 'AssetSupplySample',
+            __typename: 'AssetBorrowSample',
             date: expect.any(Date),
             amount: expect.any(Object),
             highestApy: expect.any(Object),
@@ -34,13 +35,13 @@ describe('Querying Asset Supply Apy History on Aave V4', () => {
       });
     });
 
-    describe('When fetching the asset apy supply history with a specific time window', () => {
+    describe('When fetching the asset apy borrow history with a specific time window', () => {
       const timeWindowOptions = Object.values(TimeWindow);
 
       it.each(timeWindowOptions)(
-        'Then it should return the asset apy supply history for the %s time window',
+        'Then it should return the asset apy borrow history for the %s time window',
         async (window) => {
-          const result = await assetSupplyHistory(client, {
+          const result = await assetBorrowHistory(client, {
             query: {
               token: {
                 chainId: ETHEREUM_FORK_ID,
@@ -49,12 +50,13 @@ describe('Querying Asset Supply Apy History on Aave V4', () => {
             },
             window: window,
           });
+
           assertOk(result);
 
           const { now, startDate } = getTimeWindowDates(window);
           expect(result.value).toBeArrayWithElements(
             expect.objectContaining({
-              __typename: 'AssetSupplySample',
+              __typename: 'AssetBorrowSample',
               date: expect.toBeBetweenDates(startDate, now),
               amount: expect.any(Object),
               highestApy: expect.any(Object),
