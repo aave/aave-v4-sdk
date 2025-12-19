e1e53b88cefd2481fc23bed70ca9e904215132a6
Juan Garcia <juan@aave.com>
2025-12-19 13:25:04 +0100

    fix: adapt test and add check for userSwaps positions

--- packages/spec/swap/business.spec.ts
+++ packages/spec/swap/business.spec.ts
@@ -1,5 +1,17 @@
-import { assertOk, bigDecimal, evmAddress, SwapKind } from '@aave/client';
-import { prepareSwap, swap, swapQuote } from '@aave/client/actions';
+import {
+  assertOk,
+  bigDecimal,
+  evmAddress,
+  invariant,
+  SwapKind,
+} from '@aave/client';
+import {
+  prepareSwap,
+  swap,
+  swapQuote,
+  swapStatus,
+  userSwaps,
+} from '@aave/client/actions';
 import {
   client,
   createNewWallet,
@@ -7,9 +19,9 @@ import {
   ETHEREUM_USDC_ADDRESS,
   ETHEREUM_WETH_ADDRESS,
   fundErc20Address,
-} from '@aave/client/test-utils';
+} from '@aave/client/testing';
 import { sendTransaction, signSwapTypedDataWith } from '@aave/client/viem';
-import { beforeAll, describe, it } from 'vitest';
+import { beforeAll, describe, expect, it } from 'vitest';
 
 const user = await createNewWallet(
   '0x7e97068be691cce1b5c1216b8bc4600fa9c605fcef07c8ef5af05f86e838d69b',
@@ -21,16 +33,15 @@ describe('Aave V4 Swap Scenarios', () => {
       beforeAll(async () => {
         const setup = await fundErc20Address(evmAddress(user.account.address), {
           address: ETHEREUM_USDC_ADDRESS,
-          amount: bigDecimal('200'),
+          amount: bigDecimal('20'),
           decimals: 6,
         });
         assertOk(setup);
       });
 
-      it(`Then the user's swap positions are updated`, async () => {
-        // 1. Request a quote
+      it(`Then the user's swap positions are updated`, async ({ annotate }) => {
         const swapResult = await swapQuote(client, {
-          amount: bigDecimal('100'),
+          amount: bigDecimal('20'),
           sell: { erc20: ETHEREUM_USDC_ADDRESS },
           buy: { erc20: ETHEREUM_WETH_ADDRESS },
           chainId: ETHEREUM_FORK_ID,
@@ -83,6 +94,29 @@ describe('Aave V4 Swap Scenarios', () => {
           });
 
         assertOk(swapResult);
+        invariant(
+          swapResult.value.__typename === 'SwapReceipt',
+          'Swap result is not a swap receipt',
+        );
+        annotate(`Swap id: ${swapResult.value.id}`);
+        const status = await swapStatus(client, { id: swapResult.value.id });
+        assertOk(status);
+        // Check swap was opened successfully
+        expect(status.value.__typename).toBe('SwapOpen');
+
+        const swapPositions = await userSwaps(client, {
+          chainId: ETHEREUM_FORK_ID,
+          user: evmAddress(user.account.address),
+        });
+        assertOk(swapPositions);
+        expect(
+          swapPositions.value.items.find(
+            (swap) =>
+              swap.__typename === 'SwapOpen' &&
+              swapResult.value.__typename === 'SwapReceipt' &&
+              swap.swapId === swapResult.value.id,
+          ),
+        ).toBeDefined();
       });
     });
   });
