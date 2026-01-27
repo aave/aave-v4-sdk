233dd2c4cba3997617676dc07bed205e67aa92b7
Juan Garcia <juan@aave.com>
2026-01-27 12:51:56 +0100

    fix: grammar

--- packages/spec/repay/swap.todo.ts
+++ packages/spec/repay/swap.todo.ts
@@ -1,7 +1,7 @@
 import { describe, it } from 'vitest';
 
 describe('Repay Position swapping on Aave V4', () => {
-  describe('Given a user with a supply position enable as collateral', () => {
+  describe('Given a user with a supply position enabled as collateral', () => {
     describe('And the user has a borrow position', () => {
       describe('When the user wants to repay part of the borrow position in a different token than the one they borrowed with a market order', () => {
         it.todo(
@@ -15,7 +15,7 @@ describe('Repay Position swapping on Aave V4', () => {
         );
       });
 
-      describe('And the user has a another supply position with a different token as the borrowed', () => {
+      describe('And the user has another supply position with a different token as the borrowed', () => {
         describe('When the user wants to repay part of the borrow position with the previous supply position with a market order', () => {
           it.todo(
             'Then the user should be able to repay and the position should be updated',
