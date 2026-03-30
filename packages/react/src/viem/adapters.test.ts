4d91232226d30de1bb6890c46d370b89dbcc6f83
Juan Garcia <juan@aave.com>
2026-03-30 11:55:10 +0200

    fix: add retry

--- packages/react/src/viem/adapters.test.ts
+++ packages/react/src/viem/adapters.test.ts
@@ -24,239 +24,243 @@ import { useSendTransaction } from './adapters';
 
 const user = await createNewWallet();
 
-describe(`Given the viem's '${useSendTransaction.name}' adapter hook`, () => {
-  const request: TransactionRequest = {
-    __typename: 'TransactionRequest',
-    to: evmAddress(user.account.address),
-    from: evmAddress(user.account.address),
-    data: '0x' as BlockchainData,
-    value: 0n,
-    chainId: ETHEREUM_FORK_ID,
-    operations: [],
-  };
+describe(
+  `Given the viem's '${useSendTransaction.name}' adapter hook`,
+  { retry: 3 },
+  () => {
+    const request: TransactionRequest = {
+      __typename: 'TransactionRequest',
+      to: evmAddress(user.account.address),
+      from: evmAddress(user.account.address),
+      data: '0x' as BlockchainData,
+      value: 0n,
+      chainId: ETHEREUM_FORK_ID,
+      operations: [],
+    };
 
-  describe('When the wallet is on a different chain than the TransactionRequest chain', () => {
-    let walletChainId = `0x${(42).toString(16)}`;
+    describe('When the wallet is on a different chain than the TransactionRequest chain', () => {
+      let walletChainId = `0x${(42).toString(16)}`;
 
-    const provider = setupEip1193Interceptor((request) => {
-      switch (request.method) {
-        case 'wallet_switchEthereumChain':
-          walletChainId = request.params[0].chainId;
-          return {
-            jsonrpc: '2.0',
-            id: request.id,
-            result: null,
-          };
+      const provider = setupEip1193Interceptor((request) => {
+        switch (request.method) {
+          case 'wallet_switchEthereumChain':
+            walletChainId = request.params[0].chainId;
+            return {
+              jsonrpc: '2.0',
+              id: request.id,
+              result: null,
+            };
 
-        case 'eth_chainId':
-          return {
-            jsonrpc: '2.0',
-            id: request.id,
-            result: walletChainId,
-          };
-      }
-      return;
+          case 'eth_chainId':
+            return {
+              jsonrpc: '2.0',
+              id: request.id,
+              result: walletChainId,
+            };
+        }
+        return;
+      });
+
+      const wallet = createWalletClient({
+        account: user.account,
+        transport: custom(provider),
+      });
+
+      it('Then it should switch the chain and continue', async () => {
+        const { result } = renderHookWithinContext(() =>
+          useSendTransaction(wallet),
+        );
+
+        const tx = await result.current[0](request);
+
+        assertOk(tx);
+      });
     });
 
-    const wallet = createWalletClient({
-      account: user.account,
-      transport: custom(provider),
+    describe('When the wallet does not support the TransactionRequest chain', () => {
+      let walletChainId = `0x${(42).toString(16)}`;
+
+      const provider = setupEip1193Interceptor((request) => {
+        switch (request.method) {
+          case 'wallet_switchEthereumChain':
+            return {
+              jsonrpc: '2.0',
+              id: request.id,
+              error: {
+                code: SwitchChainError.code,
+                message: 'Unrecognized chain ID',
+              },
+            };
+
+          case 'wallet_addEthereumChain':
+            walletChainId = request.params[0].chainId;
+            return {
+              jsonrpc: '2.0',
+              id: request.id,
+              result: null,
+            };
+
+          case 'eth_chainId':
+            return {
+              jsonrpc: '2.0',
+              id: request.id,
+              result: walletChainId,
+            };
+        }
+        return;
+      });
+
+      const wallet = createWalletClient({
+        account: user.account,
+        transport: custom(provider),
+      });
+
+      it('Then it should add the chain to the wallet and continue', async () => {
+        const { result } = renderHookWithinContext(() =>
+          useSendTransaction(wallet),
+        );
+
+        const tx = await result.current[0](request);
+
+        assertOk(tx);
+      });
     });
 
-    it('Then it should switch the chain and continue', async () => {
-      const { result } = renderHookWithinContext(() =>
-        useSendTransaction(wallet),
-      );
+    describe('When the wallet fails to add the chain to the wallet', () => {
+      const provider = setupEip1193Interceptor((request) => {
+        switch (request.method) {
+          case 'wallet_switchEthereumChain':
+            return {
+              jsonrpc: '2.0',
+              id: request.id,
+              error: {
+                code: SwitchChainError.code,
+                message: 'Unrecognized chain ID',
+              },
+            };
 
-      const tx = await result.current[0](request);
+          case 'wallet_addEthereumChain':
+            return {
+              jsonrpc: '2.0',
+              id: request.id,
+              error: {
+                code: MethodNotSupportedRpcError.code,
+                message: 'Resource not available',
+              },
+            };
 
-      assertOk(tx);
-    });
-  });
+          case 'eth_chainId':
+            return {
+              jsonrpc: '2.0',
+              id: request.id,
+              result: `0x${(42).toString(16)}`,
+            };
+        }
+        return;
+      });
 
-  describe('When the wallet does not support the TransactionRequest chain', () => {
-    let walletChainId = `0x${(42).toString(16)}`;
+      const wallet = createWalletClient({
+        transport: custom(provider),
+      });
 
-    const provider = setupEip1193Interceptor((request) => {
-      switch (request.method) {
-        case 'wallet_switchEthereumChain':
-          return {
-            jsonrpc: '2.0',
-            id: request.id,
-            error: {
-              code: SwitchChainError.code,
-              message: 'Unrecognized chain ID',
-            },
-          };
+      it('Then it should fail with a SigningError', async () => {
+        const { result } = renderHookWithinContext(() =>
+          useSendTransaction(wallet),
+        );
 
-        case 'wallet_addEthereumChain':
-          walletChainId = request.params[0].chainId;
-          return {
-            jsonrpc: '2.0',
-            id: request.id,
-            result: null,
-          };
+        const tx = await result.current[0](request);
 
-        case 'eth_chainId':
-          return {
-            jsonrpc: '2.0',
-            id: request.id,
-            result: walletChainId,
-          };
-      }
-      return;
-    });
-
-    const wallet = createWalletClient({
-      account: user.account,
-      transport: custom(provider),
-    });
-
-    it('Then it should add the chain to the wallet and continue', async () => {
-      const { result } = renderHookWithinContext(() =>
-        useSendTransaction(wallet),
-      );
-
-      const tx = await result.current[0](request);
-
-      assertOk(tx);
-    });
-  });
-
-  describe('When the wallet fails to add the chain to the wallet', () => {
-    const provider = setupEip1193Interceptor((request) => {
-      switch (request.method) {
-        case 'wallet_switchEthereumChain':
-          return {
-            jsonrpc: '2.0',
-            id: request.id,
-            error: {
-              code: SwitchChainError.code,
-              message: 'Unrecognized chain ID',
-            },
-          };
-
-        case 'wallet_addEthereumChain':
-          return {
-            jsonrpc: '2.0',
-            id: request.id,
-            error: {
-              code: MethodNotSupportedRpcError.code,
-              message: 'Resource not available',
-            },
-          };
-
-        case 'eth_chainId':
-          return {
-            jsonrpc: '2.0',
-            id: request.id,
-            result: `0x${(42).toString(16)}`,
-          };
-      }
-      return;
+        assertErr(tx);
+        expect(tx.error).toBeInstanceOf(SigningError);
+      });
     });
 
-    const wallet = createWalletClient({
-      transport: custom(provider),
+    describe('When the user rejects the add chain request in their wallet', () => {
+      const provider = setupEip1193Interceptor((request) => {
+        switch (request.method) {
+          case 'wallet_switchEthereumChain':
+            return {
+              jsonrpc: '2.0',
+              id: request.id,
+              error: {
+                code: SwitchChainError.code,
+                message: 'Unrecognized chain ID',
+              },
+            };
+
+          case 'wallet_addEthereumChain':
+            return {
+              jsonrpc: '2.0',
+              id: request.id,
+              error: {
+                code: UserRejectedRequestError.code,
+                message: 'User rejected the request.',
+              },
+            };
+
+          case 'eth_chainId':
+            return {
+              jsonrpc: '2.0',
+              id: request.id,
+              result: `0x${(42).toString(16)}`,
+            };
+        }
+        return;
+      });
+
+      const wallet = createWalletClient({
+        transport: custom(provider),
+      });
+
+      it('Then it should fail with a CancelError', async () => {
+        const { result } = renderHookWithinContext(() =>
+          useSendTransaction(wallet),
+        );
+
+        const tx = await result.current[0](request);
+
+        assertErr(tx);
+        expect(tx.error).toBeInstanceOf(CancelError);
+      });
     });
 
-    it('Then it should fail with a SigningError', async () => {
-      const { result } = renderHookWithinContext(() =>
-        useSendTransaction(wallet),
-      );
+    describe(`When the wallet does not support 'wallet_switchEthereumChain'`, () => {
+      const provider = setupEip1193Interceptor((request) => {
+        switch (request.method) {
+          case 'wallet_switchEthereumChain':
+            return {
+              jsonrpc: '2.0',
+              id: request.id,
+              error: {
+                code: MethodNotSupportedRpcError.code,
+                message: 'method wallet_switchEthereumChain not supported',
+              },
+            };
 
-      const tx = await result.current[0](request);
+          case 'eth_chainId':
+            return {
+              jsonrpc: '2.0',
+              id: request.id,
+              result: `0x${(42).toString(16)}`,
+            };
+        }
+        return;
+      });
 
-      assertErr(tx);
-      expect(tx.error).toBeInstanceOf(SigningError);
+      const wallet = createWalletClient({
+        transport: custom(provider),
+      });
+
+      it('Then it should fail with a SigningError', async () => {
+        const { result } = renderHookWithinContext(() =>
+          useSendTransaction(wallet),
+        );
+
+        const tx = await result.current[0](request);
+
+        assertErr(tx);
+        expect(tx.error).toBeInstanceOf(SigningError);
+      });
     });
-  });
-
-  describe('When the user rejects the add chain request in their wallet', () => {
-    const provider = setupEip1193Interceptor((request) => {
-      switch (request.method) {
-        case 'wallet_switchEthereumChain':
-          return {
-            jsonrpc: '2.0',
-            id: request.id,
-            error: {
-              code: SwitchChainError.code,
-              message: 'Unrecognized chain ID',
-            },
-          };
-
-        case 'wallet_addEthereumChain':
-          return {
-            jsonrpc: '2.0',
-            id: request.id,
-            error: {
-              code: UserRejectedRequestError.code,
-              message: 'User rejected the request.',
-            },
-          };
-
-        case 'eth_chainId':
-          return {
-            jsonrpc: '2.0',
-            id: request.id,
-            result: `0x${(42).toString(16)}`,
-          };
-      }
-      return;
-    });
-
-    const wallet = createWalletClient({
-      transport: custom(provider),
-    });
-
-    it('Then it should fail with a CancelError', async () => {
-      const { result } = renderHookWithinContext(() =>
-        useSendTransaction(wallet),
-      );
-
-      const tx = await result.current[0](request);
-
-      assertErr(tx);
-      expect(tx.error).toBeInstanceOf(CancelError);
-    });
-  });
-
-  describe(`When the wallet does not support 'wallet_switchEthereumChain'`, () => {
-    const provider = setupEip1193Interceptor((request) => {
-      switch (request.method) {
-        case 'wallet_switchEthereumChain':
-          return {
-            jsonrpc: '2.0',
-            id: request.id,
-            error: {
-              code: MethodNotSupportedRpcError.code,
-              message: 'method wallet_switchEthereumChain not supported',
-            },
-          };
-
-        case 'eth_chainId':
-          return {
-            jsonrpc: '2.0',
-            id: request.id,
-            result: `0x${(42).toString(16)}`,
-          };
-      }
-      return;
-    });
-
-    const wallet = createWalletClient({
-      transport: custom(provider),
-    });
-
-    it('Then it should fail with a SigningError', async () => {
-      const { result } = renderHookWithinContext(() =>
-        useSendTransaction(wallet),
-      );
-
-      const tx = await result.current[0](request);
-
-      assertErr(tx);
-      expect(tx.error).toBeInstanceOf(SigningError);
-    });
-  });
-});
+  },
+);
